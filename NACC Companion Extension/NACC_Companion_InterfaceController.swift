66a178b249f5794f4f9af3cd428e2cb7cd9e339f
Chris M <webmaster@magnaws.com>
2017-09-20 14:16:26 -0400

    Set up initial WatchOS files.

---
+++ NACC Companion Extension/NACC_Companion_InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  NACC_Companion_InterfaceController.swift
+//  NACC Companion Extension
+//
+//  Created by Chris Marshall on 9/20/17.
+//  Copyright © 2017 MAGSHARE. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class NACC_Companion_InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
